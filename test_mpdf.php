8aaa32b4505445609b765437d9ec55795d3102ad
CaoX <r.alan.rdz@unrealcodex.com>
2018-09-06 01:00:35 -0500

    new_

---
+++ test_mpdf.php
@@ -0,0 +1,154 @@
+<?php
+
+
+
+
+//==============================================================
+//==============================================================
+//==============================================================
+
+include("npdf/mpdf.php");
+
+$mpdf=new mPDF(); 
+
+$mpdf->WriteHTML('<table id="Tabla_01" width="369" height="592" border="0" cellpadding="0" cellspacing="0" style=" border-spacing: 0; border-collapse: collapse;" >
+	<tr>
+		<td style="padding: 0px;" colspan="11">
+			<img src="images/nueva_01.gif" width="369" height="17" alt="" ></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;" rowspan="10">
+			<img src="images/nueva_02.gif" width="17" height="574" alt="" ></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_03.gif" width="81" height="131" alt="" ></td>
+		<td style="padding: 0px;" rowspan="10">
+			<img src="images/nueva_04.gif" width="3" height="574" alt="" ></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_05.gif" width="82" height="131" alt=""></td>
+		<td style="padding: 0px;" rowspan="10">
+			<img src="images/nueva_06.gif" width="3" height="574" alt="" ></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_07.gif" width="81" height="131" alt="" ></td>
+		<td style="padding: 0px;" rowspan="10">
+			<img src="images/nueva_08.gif" width="3" height="574" alt="" ></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_09.gif" width="81" height="131" alt="" ></td>
+		<td style="padding: 0px;" colspan="2" rowspan="8">
+			<img src="images/nueva_10.gif" width="18" height="542" alt="" ></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;">
+			<img src="images/nueva_11.gif" width="81" height="4" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_12.gif" width="82" height="4" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_13.gif" width="81" height="4" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_14.gif" width="81" height="4" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;">
+			<img src="images/nueva_15.gif" width="81" height="131" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_16.gif" width="82" height="131" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_17.gif" width="81" height="131" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_18.gif" width="81" height="131" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;">
+			<img src="images/nueva_19.gif" width="81" height="4" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_20.gif" width="82" height="4" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_21.gif" width="81" height="4" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_22.gif" width="81" height="4" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;">
+			<img src="images/nueva_23.gif" width="81" height="131" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_24.gif" width="82" height="131" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_25.gif" width="81" height="131" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_26.gif" width="81" height="131" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;">
+			<img src="images/nueva_27.gif" width="81" height="4" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_28.gif" width="82" height="4" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_29.gif" width="81" height="4" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_30.gif" width="81" height="4" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;">
+			<img src="images/nueva_31.gif" width="81" height="132" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_32.gif" width="82" height="132" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/nueva_33.gif" width="81" height="132" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_34.gif" width="81" height="132" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;" rowspan="3">
+			<img src="images/nueva_35.gif" width="81" height="37" alt=""></td>
+		<td style="padding: 0px;" rowspan="3">
+			<img src="images/nueva_36.gif" width="82" height="37" alt=""></td>
+		<td style="padding: 0px;" rowspan="3">
+			<img src="images/nueva_37.gif" width="81" height="37" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_38.gif" width="81" height="5" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;" rowspan="2">
+			<img src="images/nueva_39.gif" width="39" height="32" alt=""></td>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_40.png" width="47" height="26" alt=""></td>
+		<td style="padding: 0px;" rowspan="2">
+			<img src="images/nueva_41.gif" width="13" height="32" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;" colspan="2">
+			<img src="images/nueva_42.gif" width="47" height="6" alt=""></td>
+	</tr>
+	<tr>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="17" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="81" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="3" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="82" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="3" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="81" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="3" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="39" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="42" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="5" height="1" alt=""></td>
+		<td style="padding: 0px;">
+			<img src="images/espacio.gif" width="13" height="1" alt=""></td>
+	</tr>
+</table>');
+$mpdf->Output();
+
+
+//==============================================================
+//==============================================================
+//==============================================================
+
+
+?>
